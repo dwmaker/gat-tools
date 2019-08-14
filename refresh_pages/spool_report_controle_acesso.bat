95cf1a3f009b0a61f7ba786cbfbbc1ccc16cb0ce
Paulo Ponciano <paulo.ponciano@spread.com.br>
2019-08-14 16:21:54 -0300

    ok

--- refresh_pages/spool_report_controle_acesso.bat
+++
@@ -1,6 +0,0 @@
-set NLS_LANG=.AL32UTF8
-CHCP 1252
-set nls_lang=american_america.we8mswin1252
-
-sqlplus -s -l dbanovosms/themask@ddad10g.world "@%~dpn0.sql" "%~dp0..\public\report_controle_acesso.html"
-timeout /t 10
