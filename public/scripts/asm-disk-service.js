c18308d457058fb1698528edfd47be84d7281bfa
Paulo Ponciano <paulo.ponciano@spread.com.br>
2019-08-15 16:14:56 -0300

    adding a change from the feature branch

--- public/scripts/asm-disk-service.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp').service('asm-disk-service', ['$http', 
-function($http)
-{
-	this.list = async function list(param)
-	{
-		return await $http.get("/api/v1/asmdisks?datasourceCode="+ param.datasourceCode);
-	};
-}]);
