19f1241c0c089c3efd5bcf844a623abf1e602109
Paulo Ponciano <paulo.ponciano@spread.com.br>
2019-08-19 12:34:01 -0300

    ok

--- public/components/controle-acesso/controle-acesso-controller.js
+++ public/components/controle-acesso/controle-acesso-controller.js
@@ -5,15 +5,19 @@ function($scope, $http, controleAcessoService)
 	$scope.$watch('progress');
 	
 	$scope.messages=[];
+	$scope.filtro={};
+	$scope.list={};
 	
 	$scope.datatable = {};
 	
-	$scope.filtrar = function(filter)
+	$scope.filtrar = function(filtro)
 	{
 		return function(item)
 		{
 			let ret = true;
-			if (!(item.type == filter.type)) ret = false;
+			if(typeof filtro.cd_ambiente !== "undefined") if (!(item.cd_ambiente == filtro.cd_ambiente)) return false;
+			if(typeof filtro.cd_sistema  !== "undefined") if (!(item.cd_sistema  == filtro.cd_sistema )) return false;
+			if(typeof filtro.cd_cenario  !== "undefined") if (!(item.cd_cenario  == filtro.cd_cenario )) return false;
 			return ret;
 		};
 	}
@@ -23,11 +27,28 @@ function($scope, $http, controleAcessoService)
 	.then((res)=>{$scope.metadata = res})
 	.catch((err)=>{console.error(err)})
 	
+	
+	
+	function distinct(arr)
+	{
+		let saida = [];
+		arr.forEach((item)=>
+		{
+			if(!saida.includes(item)) saida.push(item);
+		})
+		return saida;
+		
+	}
 	controleAcessoService.getAccessControls()
 	.then((res1)=>
 	{
 		$scope.datatable.displaydata = res1;
 		$scope.progress = false;
+		
+		$scope.list.cd_ambiente = distinct($scope.datatable.displaydata.map((row)=>{return row.cd_ambiente}));
+		$scope.list.cd_sistema = distinct($scope.datatable.displaydata.map((row)=>{return row.cd_sistema}));
+		$scope.list.cd_cenario = distinct($scope.datatable.displaydata.map((row)=>{return row.cd_cenario}));
+		
 		$scope.$apply()
 	})
 	.catch(
