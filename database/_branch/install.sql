aed37ef9e05a3efa7235719fecdeec5c4c1679e3
Paulo Ponciano <paulo.ponciano@spread.com.br>
2019-09-07 00:46:26 -0300

    ok

--- database/_branch/install.sql
+++ database/_branch/install.sql
@@ -1,6 +1,6 @@
-create sequence core.sq_mapa START WITH 1000;
-create sequence core.sq_mapa_servidor START WITH 1000;
-create sequence core.sq_mapa_usuario START WITH 1000;
+create sequence core.sq_mapa START WITH 1;
+create sequence core.sq_mapa_servidor START WITH 1;
+create sequence core.sq_mapa_usuario START WITH 1;
 
 create table core.tb_mapa
 (
@@ -11,11 +11,11 @@ create table core.tb_mapa
 	cd_ambiente varchar2(10) null,
 	tp_mapa char(1) not null,
 	id_mapa_template integer,
-	"NM_USUARIO_CRIACAO" VARCHAR2(30 BYTE) not null, 
-	"DT_CRIACAO" DATE not null, 
-	"NM_USUARIO_ALTERACAO" VARCHAR2(30 BYTE), 
-	"DT_ALTERACAO" DATE,
-    constraint tb_mapa_pk primary key (id_mapa)
+	NM_USUARIO_CRIACAO VARCHAR2(30 BYTE) not null, 
+	DT_CRIACAO DATE not null, 
+	NM_USUARIO_ALTERACAO VARCHAR2(30 BYTE), 
+	DT_ALTERACAO DATE,
+	constraint tb_mapa_pk primary key (id_mapa)
 );
 
 create table core.tb_mapa_servidor
@@ -25,11 +25,11 @@ create table core.tb_mapa_servidor
 	cd_tecnologia varchar2(20) not null,
 	cd_servidor varchar2(20) not null,
 	ds_conexao varchar2(255) not null, 
-	"NM_USUARIO_CRIACAO" VARCHAR2(30 BYTE) not null, 
-	"DT_CRIACAO" DATE not null, 
-	"NM_USUARIO_ALTERACAO" VARCHAR2(30 BYTE), 
-	"DT_ALTERACAO" DATE,
-    constraint tb_mapa_servidor_pk primary key (id_mapa_servidor),
+	NM_USUARIO_CRIACAO VARCHAR2(30 BYTE) not null, 
+	DT_CRIACAO DATE not null, 
+	NM_USUARIO_ALTERACAO VARCHAR2(30 BYTE), 
+	DT_ALTERACAO DATE,
+	constraint tb_mapa_servidor_pk primary key (id_mapa_servidor),
 	CONSTRAINT tb_mapa_servidor_UK1 UNIQUE (id_mapa, cd_tecnologia, cd_servidor)
 );
 create table core.tb_mapa_usuario
@@ -38,11 +38,11 @@ create table core.tb_mapa_usuario
 	id_mapa_servidor integer not null,
 	nm_usuario varchar2(40) not null,
 	ds_senha varchar2(40) not null, 
-	"NM_USUARIO_CRIACAO" VARCHAR2(30 BYTE) not null, 
-	"DT_CRIACAO" DATE not null, 
-	"NM_USUARIO_ALTERACAO" VARCHAR2(30 BYTE), 
-	"DT_ALTERACAO" DATE,
-    constraint tb_mapa_usuario_pk primary key (id_mapa_usuario),
+	NM_USUARIO_CRIACAO VARCHAR2(30 BYTE) not null, 
+	DT_CRIACAO DATE not null, 
+	NM_USUARIO_ALTERACAO VARCHAR2(30 BYTE), 
+	DT_ALTERACAO DATE,
+	constraint tb_mapa_usuario_pk primary key (id_mapa_usuario),
 	CONSTRAINT TB_MAPA_USUARIO_UK1 UNIQUE (ID_MAPA_SERVIDOR, NM_USUARIO)
 );
 
